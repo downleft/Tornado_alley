2d503931bb6f699e75dbbaaec354c38012a5c5a6
downleft <downleft@jodys-air.attlocal.net>
2024-04-04 19:45:06 -0400

    States Dropdown Created

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,11 @@
 // Populate Year dropdown menu
-for (let j = 1950; j < 2022; j++) {
+for (let j = 1950; j < 2024; j++) {
     d3.select("#selYear").append("option").attr("value", j).text(j);
   };
+
+// Populate States dropdown menu
+// States JSON format from https://gist.github.com/mshafrir/2646763#file-states_titlecase-json
+for (let i = 0; allstates.length; i++) {
+    d3.select("#selState").append("option").attr("value", allstates[i]["abbreviation"]).text(allstates[i]["name"]);
+  };
+console.log(allstates)
